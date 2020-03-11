3360fe0dd6bf007b576f6c5c503f61f1af3bf917
KariukiAbel <abelmuriithi@gmail.com>
2020-03-11 17:13:49 +0300

    Finished the AccountRecord class

--- src/com/nabesh/AccountRecord.java
+++ src/com/nabesh/AccountRecord.java
@@ -1,4 +1,57 @@
 package com.nabesh;
 
 public class AccountRecord {
+    //Declaring all class variables and instances of other classes
+    private int account;
+    private String firstName;
+    private String lastName;
+    private  double balance;
+
+    //Constructor (called when during building of the class)
+    //no-arguement constructor calls other constructor with default values
+    public AccountRecord(){
+        this(0, "", "",0.0);
+    }
+
+    //initialize record
+    public AccountRecord(int acct, String first, String last, double bal){
+        setAccount(acct);
+        setFirstName(first);
+        setLastName(last);
+        setBalance(bal);
+
+    }
+//Setters
+    public void setAccount(int acct) {
+        this.account = acct;
+    }
+
+    public void setFirstName(String first) {
+        this.firstName = first;
+    }
+
+    public void setLastName(String last) {
+        this.lastName = last;
+    }
+
+    public void setBalance(double bal) {
+        this.balance = bal;
+    }
+    //Getters
+
+    public int getAccount() {
+        return account;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
--- src/com/nabesh/CreditInquiry.java
+++ src/com/nabesh/CreditInquiry.java
@@ -1,4 +1,27 @@
 package com.nabesh;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CreditInquiry {
+    private MenuOptions accountType;
+    private Scanner input;
+
+    private final static MenuOptions [] choices = {MenuOptions.ZERO_BALANCE, MenuOptions.CREDIT_BALANCE,
+            MenuOptions.DEBIT_BALANCE, MenuOptions.EXIT};
+
+    private void readRecords(){
+        AccountRecord record = new AccountRecord();
+        try {
+            input = new Scanner(new File("clients.txt"));
+
+            while (input.hasNext()){
+                record.setAccount(input.nextInt());
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
